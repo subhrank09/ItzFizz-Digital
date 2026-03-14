9be86eba138513a06f00202dd09546ac29eaf0c4
subhrank09 <167698982+subhrank09@users.noreply.github.com>
2026-03-14 15:22:38 +0530

    Simplify next.config.ts by removing options
    Removed unnecessary configuration options from nextConfig.

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,7 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  output: 'export',
-  images: {
-    unoptimized: true,
-  },
-  // Notice there are NO slashes before basePath and assetPrefix now!
-  basePath: '/ItzFizz-Digital',
-  assetPrefix: '/ItzFizz-Digital/',
+  // Completely empty! Vercel does all the heavy lifting.
 };
 
 export default nextConfig;
